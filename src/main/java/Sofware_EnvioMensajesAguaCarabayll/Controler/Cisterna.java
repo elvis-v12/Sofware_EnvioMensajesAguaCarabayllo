792b58798b1ef2e95a53ba25c5b74c9a6c3a668f
julio2707-cesar <jingaqu@ucvvirtual.edu.pe>
2023-12-11 18:02:20 -0500

    Estoy subiendo la clase generada cisterna

--- src/main/java/Sofware_EnvioMensajesAguaCarabayll/Controler/Cisterna.java
+++ src/main/java/Sofware_EnvioMensajesAguaCarabayll/Controler/Cisterna.java
@@ -9,5 +9,40 @@ package Sofware_EnvioMensajesAguaCarabayll.Controler;
  * @author Elvis
  */
 public class Cisterna {
+    private String placa;
+    private double capacidad;
+    private int grifos;
+
+    public Cisterna(String placa, double capacidad, int grifos) {
+        this.placa = placa;
+        this.capacidad = capacidad;
+        this.grifos = grifos;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getGrifos() {
+        return grifos;
+    }
+
+    public void setGrifos(int grifos) {
+        this.grifos = grifos;
+    }
+    
+    
     
 }
